665d65fcddbd42953bf6518340da23dd0fa81735
Eugene Menski <eugene.menski@gmail.com>
2023-05-27 20:56:15 +0400

    Reworked links list and added remove an item option

--- links-storage/index.js
+++ links-storage/index.js
@@ -28,9 +28,12 @@ if (linksFromLocalStorage) {
 
 tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        let item = new Item(tabs[0].url, descriptionInputEl.value, false)
+        let item = new Item(
+                            tabs[0].url,
+                            descriptionInputEl.value,
+                            false)
         links.push(item)
-        localStorage.setItem("links", JSON.stringify(links) )
+        localStorage.setItem( "links", JSON.stringify(links) )
         render(links)
     })
 })
@@ -59,14 +62,48 @@ inputBtn.addEventListener("click", function() {
     let item
     try {
         linkInputEl.style.backgroundColor = "#ffffff"
-        item = new Item(linkInputEl.value, descriptionInputEl.value, false)
+        item = new Item(
+                        linkInputEl.value,
+                        descriptionInputEl.value,
+                        false)
         links.push(item)
         linkInputEl.value = ""
         descriptionInputEl.value = ""
-        localStorage.setItem("links", JSON.stringify(links) )
+        localStorage.setItem( "links", JSON.stringify(links) )
         render(links)
     }
     catch(err) {
         linkInputEl.style.backgroundColor = "#fa8072"
     }
 })
+
+// Add a "checked" symbol when clicking on a list item
+ulEl.addEventListener('dblclick', function(event) {
+    if (event.target.tagName === 'LI') {
+        const linkHref = event.target.children[0].getAttribute("href")
+        const description = event.target.children[0].textContent.trim()
+        const checked = true
+        const item = new Item(
+                                linkHref,
+                                description,
+                                checked)
+
+        removeStorageItem(item, linksFromLocalStorage)
+    }
+}, false)
+
+function removeStorageItem(item, localStorageItems) {
+    let newLinks = []
+    for (let i = 0; i < localStorageItems.length; i++) {
+        const localStorageItem = new Item(
+                                            localStorageItems[i].url,
+                                            localStorageItems[i].description,
+                                            localStorageItems[i].checked)
+        if (!(localStorageItem.url === item.url && item.checked === true)) {
+            newLinks.push(localStorageItem)
+        }
+    }
+    location.reload()
+    localStorage.setItem( "links", JSON.stringify(newLinks) )
+    render(newLinks)
+}
